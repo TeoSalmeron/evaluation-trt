94b57058e2bafea1951654720ad5b66c0494c369
Téo Salmeron <teo.salmeron2601@gmail.com>
2023-07-28 17:14:49 +0200

    Optimized js code

--- public/script/consulting.js
+++ public/script/consulting.js
@@ -10,26 +10,26 @@ const candidateTable = document.getElementById("candidateTable")
 
 const trs = document.getElementsByTagName("tr")
 
-for (let f of recruiterForms) {
+function listenToFormAndConfirmUser(f, role) {
     f.addEventListener("submit", (e) => {
         e.preventDefault()
         let action = f[1].value
         let id = f[0].value
+        let status = role === "candidate" ? candidateFormStatus : recruiterFormStatus
         let xhr = new XMLHttpRequest
         xhr.onreadystatechange = function() {
             if(xhr.status == 200 && xhr.readyState == 4) {
                 let response = xhr.response
-                recruiterFormStatus.innerText = response.msg
+                status.innerText = response.msg
                 if(response.error == 1) {
-                    recruiterFormStatus.style.color = "red"
+                    status.style.color = "red"
                 } else {
-                    recruiterFormStatus.style.color = "green"
+                    status.style.color = "green"
                     for (let tr of trs) {
                         if(tr.className == f.id) {
                             tr.style.display = "none"
                         }
                     }
-                    console.log(recruiterTable)
                 }
             } else if (xhr.readyState == 4) {
                 console.log("Erreur")
@@ -42,33 +42,10 @@ for (let f of recruiterForms) {
     })
 }
 
+for (let f of recruiterForms) {
+    listenToFormAndConfirmUser(f, "recruiter")
+}
+
 for (let f of candidateForms) {
-    f.addEventListener("submit", (e) => {
-        e.preventDefault()
-        let action = f[1].value
-        let id = f[0].value
-        let xhr = new XMLHttpRequest
-        xhr.onreadystatechange = function() {
-            if(xhr.status == 200 && xhr.readyState == 4) {
-                let response = xhr.response
-                candidateFormStatus.innerText = response.msg
-                if(response.error == 1) {
-                    candidateFormStatus.style.color = "red"
-                } else {
-                    candidateFormStatus.style.color = "green"
-                    for (let tr of trs) {
-                        if(tr.className == f.id) {
-                            tr.style.display = "none"
-                        }
-                    }
-                }
-            } else if (xhr.readyState == 4) {
-                console.log("Erreur")
-            }
-        }
-        xhr.open("POST", "/profil/consultant_actions", true)
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-        xhr.responseType = "json"
-        xhr.send("action=" + action + "&id=" + id)
-    })
+    listenToFormAndConfirmUser(f, "candidate")
 }
